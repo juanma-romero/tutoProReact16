6fb65fb78cff5285bfe8ea702ba0aed13af53b20
Juanma Romero <xjuanma.romerox@gmail.com>
2020-03-24 17:03:35 -0300

    ok hasta pag 27 - 48/750

---
+++ src/TodoBanner.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class TodoBanner extends Component {
+  render = () => 
+    <h4 className="bg-primary text-white text-center p-2">
+      { this.props.name }'s To Do List
+      ({ this.props.task.filter(t => !t.done).length } items to do)
+    </h4>
+}
+//
+//({ this.props.tasks.filter(t => !t.done).length } items to do)
